2a6638ab5b5b5fcb3c2e4e57663e8533b7125f8d
MPNuri <mpnurullah02@gmail.com>
2022-07-09 01:37:14 -0400

    there is more change

--- src/Futbol.java
+++ src/Futbol.java
@@ -5,5 +5,13 @@ public class Futbol {
         System.out.print("Galatasarayyyy");
         System.out.println("benim biricik sevgilim");
 
+
+        // first create a project
+        // and then create a class
+        // and then use vsc (which will turn to GIT later on) to share project on github.
+        // after that use commit comment with your explanation
+        // and lastly push your code by Push comment
+
+
     }
 }
